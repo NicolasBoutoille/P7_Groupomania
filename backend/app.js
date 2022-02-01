bd8e9635c5e51b5841aacc10815d72f91a8f4fc8
Nicolas Boutoille <nicolas.boutoille@icloud.com>
2022-02-01 18:17:29 +0100

    Add route signup

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,22 @@
 const express = require('express');
 const app = express();
-require('dotenv').config({path: './config/.env'})
-const mysql = require('mysql2');
+const db = require('./config/db');
 
-// Connexion a la base de données
-const db = mysql.createConnection({
-    host: process.env.HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-});
+// Importation des routes
+const userRoutes = require('./routes/user.routes');
 
-db.connect(function(err) {
-    if (err) throw err;
-    console.log("Connecté a la base de données MySQL !");
-});
+// // Connexion a la base de données
+// const db = mysql.createConnection({
+//     host: process.env.HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME
+// });
+
+// db.connect(function(err) {
+//     if (err) throw err;
+//     console.log("Connecté a la base de données MySQL !");
+// });
 
 // Middleware CORS
 app.use((req, res, next) => {
@@ -21,17 +24,18 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
-  });
-
-app.use('/api/user', (req, res, next) => {
-    const user = [
-        {
-            _id: '3524324',
-            name: 'Nicolas',
-            email: 'test@gmail.com',
-        },
-    ];
-    res.status(200).json(user);
 });
+
+app.use(express.json());
+
+app.use('/api/user', userRoutes);
+
+//   app.use('/api/user', (req, res, next) => {
+//     db.query('SELECT * FROM users', function (err, result) {
+//         if (err) throw error;
+//         console.log(result);
+//         res.status(200).json(result);
+//     });
+//   });
 
 module.exports = app;
