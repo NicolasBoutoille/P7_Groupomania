d589274bc129cb47a994c971774b945e9afed8dd
Nicolas Boutoille <nicolas.boutoille@icloud.com>
2022-02-11 15:32:34 +0100

    Add login and signup function to frontend

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,18 +1,36 @@
 <template>
   <div class="logo">
-    <img src="../assets/icon-left-font-monochrome-white.svg" alt="Logo Groupomania"/>
+    <img
+      src="../assets/icon-left-font-monochrome-white.svg"
+      alt="Logo Groupomania"
+    />
   </div>
   <div class="card">
     <h1 class="card_title">Connexion</h1>
-    <p class="card_subtitle">Tu n'as pas encore de compte ? <span class="card__action" @click="switchToCreateAccount()">Créer un compte</span></p>
+    <p class="card_subtitle">
+      Tu n'as pas encore de compte ?
+      <span class="card__action" @click="switchToCreateAccount()"
+        >Créer un compte</span
+      >
+    </p>
     <div class="form-row">
-      <input v-model="email" class="form-row__input" type="text" placeholder="Adresse mail"/>
+      <input
+        v-model="email"
+        class="form-row__input"
+        type="text"
+        placeholder="Adresse mail"
+      />
     </div>
     <div class="form-row">
-      <input v-model="password" class="form-row__input" type="password" placeholder="Mot de passe">
+      <input
+        v-model="password"
+        class="form-row__input"
+        type="password"
+        placeholder="Mot de passe"
+      />
     </div>
     <div class="form-row">
-      <button class="button" :class="{'button--disabled' : !validatedFields}">
+      <button @click="login()" class="button" :class="{ 'button--disabled': !validatedFields }">
         Connexion
       </button>
     </div>
@@ -21,54 +39,67 @@
 
 <script>
 export default {
-  name: 'Login',
+  name: "Login",
   data: function () {
     return {
-      email: '',
-      password: '',
-    }
+      email: "",
+      password: "",
+    };
   },
   computed: {
     validatedFields: function () {
       if (this.email != "" && this.password != "") {
-          return true;
-        } else {
-          return false;
-        }
+        return true;
+      } else {
+        return false;
+      }
     },
   },
   methods: {
     switchToCreateAccount: function () {
-      window.location.href="http://localhost:8080/#/signup";
+      window.location.href = "http://localhost:8080/#/signup";
     },
-    createAccount: function () {
-      console.log(this.email, this.username, this.password);
+    login: function () {
+      const data = {
+        email: this.email,
+        password: this.password,
+      };
+      fetch("http://localhost:3000/api/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      })
+        .then((res) => res.json())
+        .then((res) => localStorage.setItem("token", res.token))
+        .catch((err) => console.log(err));
     },
-  }
-}
+  },
+};
 </script>
 
 <style scoped>
-  .form-row {
-    display: flex;
-    margin: 16px 0px;
-    gap: 16px;
-    flex-wrap: wrap;
-  }
+.form-row {
+  display: flex;
+  margin: 16px 0px;
+  gap: 16px;
+  flex-wrap: wrap;
+}
 
-  .form-row__input {
-    padding: 8px;
-    border: none;
-    border-radius: 8px;
-    background: #f2f2f2;
-    font-weight: 500;
-    font-size: 16px;
-    flex: 1;
-    min-width: 100px;
-    color: black;
-  }
+.form-row__input {
+  padding: 8px;
+  border: none;
+  border-radius: 8px;
+  background: #f2f2f2;
+  font-weight: 500;
+  font-size: 16px;
+  flex: 1;
+  min-width: 100px;
+  color: black;
+}
 
-  .form-row__input::placeholder {
-    color: #aaaaaa;
-  }
+.form-row__input::placeholder {
+  color: #aaaaaa;
+}
 </style>
--- frontend/src/views/Signup.vue
+++ frontend/src/views/Signup.vue
@@ -1,21 +1,46 @@
 <template>
-    <div class="logo">
-    <img src="../assets/icon-left-font-monochrome-white.svg" alt="Logo Groupomania"/>
+  <div class="logo">
+    <img
+      src="../assets/icon-left-font-monochrome-white.svg"
+      alt="Logo Groupomania"
+    />
   </div>
   <div class="card">
     <h1 class="card_title">Inscription</h1>
-    <p class="card_subtitle">Tu as déjà un compte ? <span class="card__action" @click="switchToLogin()">Se connecter</span></p>
+    <p class="card_subtitle">
+      Tu as déjà un compte ?
+      <span class="card__action" @click="switchToLogin()">Se connecter</span>
+    </p>
     <div class="form-row">
-      <input v-model="email" class="form-row__input" type="text" placeholder="Adresse mail"/>
+      <input
+        v-model="email"
+        class="form-row__input"
+        type="text"
+        placeholder="Adresse mail"
+      />
     </div>
     <div class="form-row">
-      <input v-model="username" class="form-row__input" type="text" placeholder="Nom d'utilisateur">
+      <input
+        v-model="username"
+        class="form-row__input"
+        type="text"
+        placeholder="Nom d'utilisateur"
+      />
     </div>
     <div class="form-row">
-      <input v-model="password" class="form-row__input" type="password" placeholder="Mot de passe">
+      <input
+        v-model="password"
+        class="form-row__input"
+        type="password"
+        placeholder="Mot de passe"
+      />
     </div>
     <div class="form-row">
-      <button @click="createAccount()" class="button" :class="{'button--disabled' : !validatedFields}">
+      <button
+        @click="createAccount()"
+        class="button"
+        :class="{ 'button--disabled': !validatedFields }"
+      >
         Créer mon compte
       </button>
     </div>
@@ -24,13 +49,13 @@
 
 <script>
 export default {
-  name: 'Signup',
+  name: "Signup",
   data: function () {
     return {
-      email: '',
-      username: '',
-      password: '',
-    }
+      email: "",
+      username: "",
+      password: "",
+    };
   },
   computed: {
     validatedFields: function () {
@@ -43,37 +68,59 @@ export default {
   },
   methods: {
     switchToLogin: function () {
-      window.location.href="http://localhost:8080/#/"
+      window.location.href = "http://localhost:8080/#/";
     },
-    createAccount: function () {
-      console.log(this.email, this.username, this.password);
-    },
-  }
-}
+    // createAccount: function () {
+    //   this.$store.dispatch('createAccount', {
+    //     email : this.email,
+    //     username: this.username,
+    //     password: this.password
+    //   }).then(function (response) {
+    //     console.log(response);
+    //   }).
+    // },
+    createAccount () {
+      const data = {
+        email : this.email,
+        username : this.username,
+        password : this.password
+      };
+      fetch('http://localhost:3000/api/signup', {
+        method: "POST",
+        headers: { 
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data),
+      })
+      .then(res => res.json())
+      .catch((error) => console.log(error))
+    }
+  },
+};
 </script>
 
 <style scoped>
-  .form-row {
-    display: flex;
-    margin: 16px 0px;
-    gap: 16px;
-    flex-wrap: wrap;
-  }
+.form-row {
+  display: flex;
+  margin: 16px 0px;
+  gap: 16px;
+  flex-wrap: wrap;
+}
 
-  .form-row__input {
-    padding: 8px;
-    border: none;
-    border-radius: 8px;
-    background: #f2f2f2;
-    font-weight: 500;
-    font-size: 16px;
-    flex: 1;
-    min-width: 100px;
-    color: black;
-  }
+.form-row__input {
+  padding: 8px;
+  border: none;
+  border-radius: 8px;
+  background: #f2f2f2;
+  font-weight: 500;
+  font-size: 16px;
+  flex: 1;
+  min-width: 100px;
+  color: black;
+}
 
-  .form-row__input::placeholder {
-    color: #aaaaaa;
-  }
+.form-row__input::placeholder {
+  color: #aaaaaa;
+}
 </style>
 
